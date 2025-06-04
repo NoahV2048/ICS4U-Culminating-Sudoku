3ff1a338050b8b1e2fb284cc7e9700f285147727
NoahV2048 <noahverdon2008@gmail.com>
2025-06-04 15:14:56 -0400

    further into the SWING of things

--- SwingTest.java
+++ SwingTest.java
@@ -1,17 +1,31 @@
-import javax.swing.JFrame;
-import javax.swing.ImageIcon;
+import javax.swing.*;
 
 public class SwingTest {
     public static void main(String[] args) {
+        // learned basics here
+        // https://www.youtube.com/playlist?list=PLNb_-KeTZieW3DNI2aLOAYBnjx29CN8dT
+
+        // create text input
+        JTextField yo = new JTextField();
+
+        // create label
+        JLabel title = new JLabel("SUDOKU SOLVER");
+        title.setVerticalAlignment(JLabel.TOP);
+        title.setHorizontalAlignment(JLabel.CENTER);
+
+        // create image
+        // ImageIcon image = new ImageIcon("SudokuSolverLogo.png");
+
         // create window
         JFrame puzzleBox = new JFrame();
         puzzleBox.setTitle("Sudoku");
         puzzleBox.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        puzzleBox.setResizable(false);
+        // puzzleBox.setResizable(false);
         puzzleBox.setSize(500, 500);
         puzzleBox.setVisible(true);
 
-       // ImageIcon image = new ImageIcon("SudokuSolverLogo");
-       // puzzleBox.setIconImage(image.getImage());
+        puzzleBox.add(title);
+        puzzleBox.add(yo);
+        // puzzleBox.setIconImage(image.getImage());
     }
 }
