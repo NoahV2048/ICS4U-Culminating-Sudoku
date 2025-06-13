61c564d7b345dbd710c47e0403a08175ba99ace5
NoahV2048 <noahverdon2008@gmail.com>
2025-06-13 13:54:53 -0400

    swing almost done

---
+++ SwingPuzzle.java
@@ -0,0 +1,101 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SwingPuzzle extends JFrame {
+    // learned basics here
+    // https://youtu.be/Kmgo00avvEw?si=ZAOYPNNYAb-rIGEX
+
+    int size;
+    int pixelSize = 450;
+    SwingSquare[][] squareValues;
+
+
+    // Constructor
+    public SwingPuzzle(int size) {
+        this.size = size;
+        setTitle("Sudoku Solver");
+        setSize(pixelSize, pixelSize);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false); // can be toggled TODO
+
+        // create title at top of program
+        JLabel title = new JLabel("SUDOKU SOLVER");
+        title.setVerticalAlignment(JLabel.TOP);
+        title.setHorizontalAlignment(JLabel.CENTER);
+        add(title);
+
+        // make layouts
+        GridLayout macroLayout = new GridLayout(size, size); // macro
+        macroLayout.setHgap(pixelSize/100);
+        macroLayout.setVgap(pixelSize/100);
+
+        GridLayout microLayout = new GridLayout(size, size); // micro
+        microLayout.setHgap(1);
+        microLayout.setVgap(1);
+
+        // create panel for the whole puzzle
+        JPanel puzzlePanel = new JPanel();
+        add(puzzlePanel);
+        puzzlePanel.setLayout(macroLayout); // grid layout
+        puzzlePanel.setSize(pixelSize/size, pixelSize/size); // TODO see if doubles work
+
+        // create smaller panels inside the main puzzle panel
+        JPanel[][] boxPanels = new JPanel[size][size];
+        for (int macroRow = 0; macroRow < size; macroRow++) {
+            for (int macroCol = 0; macroCol < size; macroCol++) {
+                // create smaller panels
+                JPanel boxPanel = new JPanel();
+                boxPanel.setLayout(microLayout); // micro grid layout
+                boxPanel.setSize(pixelSize/(size*size), pixelSize/(size*size));
+                puzzlePanel.add(boxPanel); // add each smaller panel to the larger one
+                boxPanels[macroRow][macroCol] = boxPanel;
+            }
+        }
+
+        // create text input boxes
+        squareValues = new SwingSquare[size*size][size*size];
+
+        for (int row = 0; row < size*size; row++) {
+            for (int col = 0; col < size*size; col++) {
+                // create a new SwingSquare
+                SwingSquare newSwingSquare = new SwingSquare(size);
+
+                // add SwingSquare to array of squareValues and also to the appropriate boxPanel
+                squareValues[row][col] = newSwingSquare;
+
+                int macroRow = (row/size), macroCol = (col/size);
+                boxPanels[macroRow][macroCol].add(newSwingSquare);
+            }
+        }
+
+        // create image TODO
+        // ImageIcon image = new ImageIcon("SudokuSolverLogo.png");
+        // puzzleBox.setIconImage(image.getImage());
+
+        // finally, set the frame visible
+        setVisible(true);
+    }
+
+
+    // Getters
+    public Square[][] toArray() {
+        int[][] tempIntArray = new int[size*size][size*size];
+
+        // iterate through squares
+        for (int row = 0; row < size*size; row++) {
+            for (int col = 0; col < size*size; col++) {
+                // error handling
+                // if a square is empty, assume it is zero
+                try {
+                    tempIntArray[row][col] = squareValues[row][col].getInt();
+                }
+                catch (Exception e) {
+                    tempIntArray[row][col] = 0;
+                }
+            }
+        }
+
+        // turn the temporary int array into a Square array
+        return Square.intsToSquares(tempIntArray);
+    }
+}
